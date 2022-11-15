b4318c0beeec2e4d5a858af6242b4768602a035c
Faisal Ramadhan <faisalramadhan041201@gmail.com>
2022-11-15 14:45:39 +0700

    mengubah kodingan untuk mem-filter kata-kata kasar saat user menginput value

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 # latihan git dan program python pertamaku
 
+def forbiddenWords():
+    forbidden_words = ["fuck", "shit", "asshole", "dick", "pussy"]
+    return forbidden_words
+
 def showMessage():
-    message = "Hello World"
-    return message
+    isForbiddenWord = False
+
+    while isForbiddenWord != True:
+        user_input = input("input your word: ").lower()
+        if user_input in forbiddenWords():
+            isForbiddenWord = True
+            while isForbiddenWord:
+                errorMsg = ValueError("please input correctly!")
+                new_user_input = input("input again your word: ")
+                if new_user_input in forbiddenWords():
+                    print(errorMsg)
+                    continue
+                else: return new_user_input
+        else: return user_input
 print(showMessage())
